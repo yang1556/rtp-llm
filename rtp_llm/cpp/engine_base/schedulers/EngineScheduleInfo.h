06faf5f2bd088d67bdaefaa3cac72a25b9e0d918
luoli.hn <luoli.hn@taobao.com>
2026-03-04 18:07:25 +0800

    refactor - not use request_id as worker_status version

--- rtp_llm/cpp/engine_base/schedulers/EngineScheduleInfo.h
+++ rtp_llm/cpp/engine_base/schedulers/EngineScheduleInfo.h
@@ -14,11 +14,12 @@ struct EngineScheduleInfo {
         int64_t waiting_time_ms;
         int64_t iterate_count = 0;
         int64_t end_time_ms   = -1;
-        bool    is_waiting = true;
+        bool    is_waiting    = true;
     };
     std::vector<TaskInfo> running_task_info_list;
     std::vector<TaskInfo> finished_task_info_list;
     int64_t               last_schedule_delta;
+    int64_t               latest_finished_version = 0;
 };
 
 }  // namespace rtp_llm
