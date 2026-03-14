c9b01227dfbb57f59a80b9625e250fdcd24bb32c
baowending.bwd@alibaba-inc.com <baowending.bwd@alibaba-inc.com>
2026-03-14 14:06:28 +0800

    fix - split fake load balance op

--- rtp_llm/models_py/modules/__init__.py
+++ rtp_llm/models_py/modules/__init__.py
@@ -4,6 +4,7 @@ from rtp_llm.models_py.modules.base import (
     AddBiasResLayerNormTorch,
     Embedding,
     EmbeddingBert,
+    FakeBalanceExpert,
     FusedQKRMSNorm,
     FusedSiluAndMul,
     GroupTopK,
@@ -47,6 +48,7 @@ __all__ = [
     "RMSResNorm",
     "SelectTopk",
     "GroupTopK",
+    "FakeBalanceExpert",
     "FusedSiluAndMul",
     "IndexerOp",
     # Factory modules
