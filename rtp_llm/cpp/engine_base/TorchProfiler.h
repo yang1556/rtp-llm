f1f042fe5075f3c5e190013a90c0eb462500905c
luoli.hn <luoli.hn@taobao.com>
2026-03-16 11:56:33 +0800

    feature -add cpu profile

--- rtp_llm/cpp/engine_base/TorchProfiler.h
+++ rtp_llm/cpp/engine_base/TorchProfiler.h
@@ -6,10 +6,10 @@ namespace rtp_llm {
 
 namespace tpi = torch::profiler::impl;
 
-class CudaProfiler {
+class TorchProfile {
 public:
-    CudaProfiler(const std::string& prefix, std::string user_torch_cuda_profiler_dir = "");
-    ~CudaProfiler();
+    TorchProfile(const std::string& prefix, std::string user_torch_cuda_profiler_dir = "");
+    ~TorchProfile();
     void start();
     void stop();
 
@@ -18,7 +18,7 @@ protected:
     static size_t               count;
     std::string                 prefix_;
     tpi::ProfilerConfig         config_ = tpi::ProfilerConfig(tpi::ProfilerState::KINETO);
-    std::set<tpi::ActivityType> activities_{tpi::ActivityType::CUDA};
+    std::set<tpi::ActivityType> activities_{tpi::ActivityType::CPU, tpi::ActivityType::CUDA};
     bool                        stoped_ = true;
 };
 
