b1f4d088e147d4b7f38db04455af4fc5ca284c94
hxy0118 <173128549@qq.com>
2026-03-27 15:54:40 +0800

    fixed gen_timeline no prefill data

--- rtp_llm/cpp/normal_engine/NormalEngine.cc
+++ rtp_llm/cpp/normal_engine/NormalEngine.cc
@@ -398,8 +398,6 @@ absl::Status NormalEngine::step() {
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 
-    step_profiler_.tick();
-
     list<GenerateStreamPtr> streams;
     if (device_->getDeviceProperties().tp_rank == 0 && !ffn_disaggregate_config.is_ffn_service()) {
         {
@@ -414,6 +412,13 @@ absl::Status NormalEngine::step() {
             return absl::OkStatus();
         }
     }
+
+    // tick() is called after schedule() but before process() to ensure the profiler
+    // is started before forward execution. This fixes a race condition where a
+    // gen_timeline request could be scheduled and its prefill executed before the
+    // profiler had a chance to start (configure() on gRPC thread, tick() on engine thread).
+    step_profiler_.tick();
+
     RTP_LLM_LOG_DEBUG(__PRETTY_FUNCTION__);
     int64_t      step_begin_time_us = autil::TimeUtility::currentTimeInMicroSeconds();
     absl::Status status             = absl::OkStatus();
