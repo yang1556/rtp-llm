0d3f0370d7ad07bfcec2b2b594c1fb50f1dd3244
Nancheng <yangchengjun.ycj@alibaba-inc.com>
2026-03-30 23:31:57 +0800

    refactor - copy attn_input in cp sparse mla

--- rtp_llm/models_py/bindings/OpDefs.cc
+++ rtp_llm/models_py/bindings/OpDefs.cc
@@ -123,7 +123,8 @@ void registerPyOpDefs(pybind11::module& m) {
         .def_readwrite("cache_store_inputs", &PyAttentionInputs::cache_store_inputs)
         .def_readwrite("context_parallel_info", &PyAttentionInputs::context_parallel_info)
         .def("__repr__", [](const PyAttentionInputs& self) { return "PyAttentionInputs"; })
-        .def_readwrite("prefill_cuda_graph_copy_params", &PyAttentionInputs::prefill_cuda_graph_copy_params);
+        .def_readwrite("prefill_cuda_graph_copy_params", &PyAttentionInputs::prefill_cuda_graph_copy_params)
+        .def("__copy__", [](const PyAttentionInputs& self) { return PyAttentionInputs(self); });
 
     pybind11::class_<BertEmbeddingInputs>(m, "BertEmbeddingInputs")
         .def(pybind11::init<>())
